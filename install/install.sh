0ed256b6079f2c795aa73f65ff0e4a5b140393b8
CaioslppUO <caioslppuo@gmail.com>
2021-07-22 13:07:14 -0300

    Install updated with catkin commands.

--- install/install.sh
+++ install/install.sh
@@ -12,21 +12,24 @@ HOME=/home/$USER
 BASHRC=$HOME/.bashrc
 ZSHRC=$HOME/.zshrc
 AGROBOT=$HOME/Agrobot
-AGROBOT_ENV=$AGROBOT/env
+AGROBOT_ENV=$AGROBOT/agrobot_env
 AGROBOT_ENV_BIN=$AGROBOT_ENV/bin
+CATKIN=$AGROBOT/catkin_ws
+CATKIN_SRC=$CATKIN/src
+CATKIN_DEVEL=$CATKIN/devel
 
 # Folders
 rm -rf $AGROBOT
 mkdir $AGROBOT/
 
-rm -rf $AGROBOT_ENV
 mkdir $AGROBOT_ENV
+mkdir -p $CATKIN/src/
 
 # Virtual Env
-python3 -m venv $AGROBOT_ENV/
+python3 -m venv $AGROBOT_ENV/ 
 echo "source /opt/ros/noetic/setup.zsh 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
 echo "source /opt/ros/noetic/setup.bash 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
-echo "alias exit_agrobot=deactivate" >> $AGROBOT_ENV_BIN/activate
+echo "alias agrobot=exit" >> $AGROBOT_ENV_BIN/activate
 
 AGROBOT_ALIAS="alias agrobot='source $AGROBOT_ENV_BIN/activate'"
 AGROBOT_ALIAS_GREP="$(grep 'alias agrobot' $BASHRC)"
@@ -38,6 +41,13 @@ else
     echo "alias agrobot='source $AGROBOT_ENV_BIN/activate'" >> $BASHRC
     echo "alias agrobot='source $AGROBOT_ENV_BIN/activate'" >> $ZSHRC
 fi
+
+# ROS
+cd $CATKIN && catkin_make
+echo "source $CATKIN_DEVEL/setup.bash 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
+echo "source $CATKIN_DEVEL/setup.zsh 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
+cd $CATKIN_SRC && catkin_create_pkg agrobot std_msgs rospy roscpp message_generation message_runtime
+cd $CATKIN && catkin_make
 
 # Post install
 clear
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -3,9 +3,15 @@
 # Paths
 HOME=/home/$USER
 AGROBOT=$HOME/Agrobot
+BASHRC=$HOME/.bashrc
+ZSHRC=$HOME/.zshrc
 
 # Folders
 rm -rf $AGROBOT
+
+# Profiles
+grep -v "alias agrobot" $BASHRC > temp && mv temp $BASHRC # Remove agrobot alias from .bashrc file
+grep -v "alias agrobot" $ZSHRC > temp && mv temp $ZSHRC
 
 # Post uninstall
 clear
