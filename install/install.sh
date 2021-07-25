2c6cdbce14169aaf9e1b55b63890148191f97fae
CaioslppUO <caioslppuo@gmail.com>
2021-07-25 17:48:46 -0300

    Automatization for roslaunch creation.

--- install/install.sh
+++ install/install.sh
@@ -3,24 +3,27 @@
 # Dependencies
 sudo apt install python3-pip
 sudo apt install -y python3-venv
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install -g yarn
 
 # Paths
+LOCAL_FOLDER=$PWD
 HOME=/home/$USER
 BASHRC=$HOME/.bashrc
 ZSHRC=$HOME/.zshrc
-AGROBOT=$HOME/Agrobot
-AGROBOT_ENV=$AGROBOT/agrobot_env
+AGROBOT_FOLDER=$HOME/Agrobot
+AGROBOT_ENV=$AGROBOT_FOLDER/agrobot_env
 AGROBOT_ENV_BIN=$AGROBOT_ENV/bin
-CATKIN=$AGROBOT/catkin_ws
+CATKIN=$AGROBOT_FOLDER/catkin_ws
 CATKIN_SRC=$CATKIN/src
 CATKIN_DEVEL=$CATKIN/devel
+AGROBOT=$CATKIN_SRC/agrobot
+AGROBOT_SRC=$AGROBOT/src
 
 # Folders
-rm -rf $AGROBOT
-mkdir $AGROBOT/
+rm -rf $AGROBOT_FOLDER
+mkdir $AGROBOT_FOLDER/
 
 mkdir $AGROBOT_ENV
 mkdir -p $CATKIN/src/
@@ -43,12 +46,29 @@ else
 fi
 
 # ROS
+## Catkin
 cd $CATKIN && catkin_make
 echo "source $CATKIN_DEVEL/setup.bash 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
 echo "source $CATKIN_DEVEL/setup.zsh 2>/dev/null" >> $AGROBOT_ENV_BIN/activate
 cd $CATKIN_SRC && catkin_create_pkg agrobot std_msgs rospy roscpp message_generation message_runtime
 cd $CATKIN && catkin_make
 
+## Scripts
+cd "$LOCAL_FOLDER/../scripts/" && chmod +x ./* &&  cp -r ./* "$AGROBOT_SRC/"
+
+## Roslaunch
+
+cd $AGROBOT
+mkdir launch && cd launch
+echo "<launch>" > run.launch
+files=$(ls $LOCAL_FOLDER/../scripts)
+for entry in $files
+do
+    name=$(echo "$entry" | cut -f 1 -d '.')
+    echo "    <node pkg='agrobot' type='$entry' name='$name' output='screen'/>" >> run.launch
+done
+echo "</launch>" >> run.launch
+
 # Post install
-clear
+#clear
 echo DONE
--- internal_server/teste.cpp
+++ internal_server/teste.cpp
@@ -1,27 +1,34 @@
-#include "internal_server/client.cpp"
+#include "client.cpp"
 #include <thread>
 #include <string.h>
 
 using namespace std;
 
-void keepReceiving() {
-    while(true) {
+void keepReceiving()
+{
+    while (true)
+    {
         cout << "Mensagem recebida: " << Client::receive() << endl;
     }
 }
 
-int main() {
+int main()
+{
     Client::connect();
     char aux[10000];
     string msg2;
-    std::thread read_t (keepReceiving);
-    try {
-        while(true) {
+    std::thread read_t(keepReceiving);
+    try
+    {
+        while (true)
+        {
             cout << "digite: " << endl;
             cin >> aux;
             Client::emit(aux);
         }
-    }catch(exception e) {
+    }
+    catch (exception e)
+    {
         Client::closeConnection();
         read_t.join();
         cout << "except" << endl;
