09f19131926edf65fff4be06124185571f963137
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-08-09 15:27:22 -0300

    Correction of the logic that controls the engines

--- modules/communication/teste.py
+++ modules/communication/teste.py
@@ -1,13 +1,17 @@
 import os
 import threading
-from server import *
+import rospy
+from server import wait_for_connection, connect
 
 
-def send():
-    while True:
-        msg = input('Digite algo para enviar: ')
-        for con in connections:
-            con.send(msg.encode('utf-8'))
+connections = []
+address = []
+s = connect('localhost', 3000)
+
+
+def send(msg):
+    for con in connections:
+        con.send(msg.encode('utf-8'))
 
 
 def storeConnections():
@@ -19,26 +23,27 @@ def storeConnections():
 
 
 def startThreadMotor(port: str):
-    t_motor = threading.Thread(target=startMotor, args=(str(port)))
-    t_motor.start()
+    global t_motor
+    t_motor.append(threading.Thread(target=startMotor, args=[port]))
+    t_motor[len(t_motor)-1].start()
 
 
-def startMotor(port: str):
-    os.system("./controller.out " + port)
+def startMotor(args):
+    os.system("./controller.out " + args)
 
 
-connections = []
-address = []
-s = connect('localhost', 3000)
-
-try:
-    startThreadMotor("/dev/ttyACM0")
-    # startThreadMotor("/dev/ttyACM1")
-    t_connections = threading.Thread(target=storeConnections, args=())
-    t_connections.start()
-
-    send()
-except Exception as e:
-    for con in connections:
-        con.close()
-    print(str(e))
+t_motor = []
+if __name__ == "__main__":
+    try:
+        t_connections = threading.Thread(target=storeConnections, args=())
+        t_connections.start()
+        startThreadMotor("/dev/ttyACM0")
+        startThreadMotor("/dev/ttyACM1")
+        rospy.Subscriber('/control_robot', Control, send)
+        rospy.spin()
+    except Exception as e:
+        for u_motor in t_motor:
+            u_motor.close()
+        for con in connections:
+            con.close()
+        print(str(e))
